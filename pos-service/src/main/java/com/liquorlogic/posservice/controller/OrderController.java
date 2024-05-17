5d6c8538797597b73ce1863d24d4695e4c5a76af
Imalka Gayani Fernando <101160334+isneha44@users.noreply.github.com>
2024-05-17 21:47:26 +0530

    further updated

--- pos-service/src/main/java/com/liquorlogic/posservice/controller/OrderController.java
+++ pos-service/src/main/java/com/liquorlogic/posservice/controller/OrderController.java
@@ -125,12 +125,14 @@ public class OrderController {
         try {
             loggerLog4J.info("End findByOrderId ");
             Optional<Order> order = orderService.findByOrderId(orderId);
+//
+//            if (order.isPresent()) {
+//                return ResponseEntity.ok(Optional.of(order.get()));
+//            } else {
+//                return ResponseEntity.notFound().build();
+//            }
 
-            if (order.isPresent()) {
-                return ResponseEntity.ok(Optional.of(order.get()));
-            } else {
-                return ResponseEntity.notFound().build();
-            }
+
         } catch (Exception e) {
             handleException(e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
